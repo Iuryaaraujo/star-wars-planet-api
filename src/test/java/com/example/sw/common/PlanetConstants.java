3f1fa843c3949bf2dfdf64bc0133a9b1e9484179
Iury Araujo <iurymiraima@gmail.com>
2024-02-24 18:33:44 -0300

    Using Mockito for unit testing

---
+++ src/test/java/com/example/sw/common/PlanetConstants.java
@@ -0,0 +1,8 @@
+package com.example.sw.common;
+
+import com.example.sw.domain.Planet;
+
+public class PlanetConstants {
+    public static final Planet PLANET = new Planet("name", "climate", "terrain");
+    public static final Planet INVALID_PLANET = new Planet("", "", "");
+}
--- src/test/java/com/example/sw/domain/PlanetServiceTest.java
+++ src/test/java/com/example/sw/domain/PlanetServiceTest.java
@@ -1,17 +1,49 @@
 package com.example.sw.domain;
 
+import static com.example.sw.common.PlanetConstants.INVALID_PLANET;
+import static com.example.sw.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+// aqui só vai passar apenas as class que eu quero que seja carregada
+// @SpringBootTest(classes = PlanetService.class)
 
-@SpringBootTest
+//@SpringBootTest(classes = PlanetService.class)
+@ExtendWith(MockitoExtension.class)
 public class PlanetServiceTest {
-    @Autowired
+    //@Autowired
+    @InjectMocks
     private PlanetService planetService;
 
-    // operacao_estado_returno
-    @Test
-    public void createPlanet_WithValidData_returnsPlanet() {
+    //@MockBean
+    @Mock
+    private PlanetRepository planetRepository;
 
+
+    @Test
+    // operacao_estado_returno
+    public void createPlanet_WithValidData_returnsPlanet() {
+        when(planetRepository.save(PLANET)).thenReturn(PLANET);// when quando
+
+        Planet sut = planetService.create(PLANET);
+
+        assertThat(sut).isEqualTo(PLANET);
+    }
+
+    @Test
+    // operacao_estado_returno
+    public void createPlanet_WithInvalidData_throwsException() {
+        when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+
+        assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
     }
 }
