1a0d8c2c2b190bc826a45e2f6f4dc4c455cfb1df
Iury Araujo <iurymiraima@gmail.com>
2024-02-24 15:56:23 -0300

    Creation of the planets API

---
+++ src/test/java/com/example/sw/domain/PlanetServiceTest.java
@@ -0,0 +1,17 @@
+package com.example.sw.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PlanetServiceTest {
+    @Autowired
+    private PlanetService planetService;
+
+    // operacao_estado_returno
+    @Test
+    public void createPlanet_WithValidData_returnsPlanet() {
+
+    }
+}
